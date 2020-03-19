b3657add07b58a68559cfcf583e8bf0e342121e1
Jessica Lima <jessicajlr@gmail.com>
2020-03-19 00:17:55 -0300

    meu primeiro commit adicionei arquivo

---
+++ trabalhando_com_array.php
@@ -0,0 +1,7 @@
+/* O array anexo representa os itens de uma lista de compras.
+Quero que estes itens sejam impressos de forma a trazer uma lista de compras legível.
+Algo tipo:
+ITEM QUANTIDADE
+- Arroz : 2
+- Feijão : 3
+etc */
