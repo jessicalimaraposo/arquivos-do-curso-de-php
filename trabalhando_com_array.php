9f0046bf9ae9e402baf8e1b07885c65f0ca37a10
Jessica Lima <jessicajlr@gmail.com>
2020-03-19 00:31:40 -0300

    Hello
    world

--- trabalhando_com_array.php
+++ trabalhando_com_array.php
@@ -1,3 +1,5 @@
+
+<?php
 /* O array anexo representa os itens de uma lista de compras.
 Quero que estes itens sejam impressos de forma a trazer uma lista de compras legível.
 Algo tipo:
@@ -5,3 +7,14 @@ ITEM QUANTIDADE
 - Arroz : 2
 - Feijão : 3
 etc */
+$alimentos = array(
+    "feijao" => "feijao",
+    "arroz" => "arroz",
+    "leite" =>"leite",
+    "batata"=>"batata",
+    "ovos"=>"ovos",
+    "tomate"=>"tomate",
+);
+
+var_dump($array);
+?>
